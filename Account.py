8ff08b61ec57423904be5fe70e9606b7fe68cdb3
kayqueGovetri <govetrikayque@gmail.com>
2021-02-08 22:51:14 -0300

    Encapsulation of class attributes.

--- Account.py
+++ Account.py
@@ -3,16 +3,16 @@ class Account:
 
     def __init__(self, number, holder, balance, limit = 1000):
         print("Construindo objeto ... {}".format(self))
-        self.number = number
-        self.holder = holder
-        self.balance = balance
-        self.limit = limit
+        self.__number = number
+        self.__holder = holder
+        self.__balance = balance
+        self.__limit = limit
 
     def bank_statement(self):
-        print("The current bank balance is ${}".format(self.balance))
+        print("The current bank balance is ${}".format(self.__balance))
 
     def deposit(self, value):
-        self.balance += value
+        self.__balance += value
 
     def withdraw(self, value):
-        self.balance -= value
+        self.__balance -= value
